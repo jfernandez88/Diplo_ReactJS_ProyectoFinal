d451c78670ebc7411838f5f313e2e6a502868623
Jonathan Fernandez <jonathanfernandez88@hotmail.com>
2022-08-24 20:04:02 -0300

    Se agrega envio de mail

--- tusfondos/backend/routes/api.js
+++ tusfondos/backend/routes/api.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var noticiasModel = require('./../models/noticiasModel');
 var cloudinary = require('cloudinary').v2;
+var nodemailer = require('nodemailer');
 
 router.get('/noticias', async function (req, res, next) {
   var noticias = await noticiasModel.getNoticias();
@@ -25,6 +26,33 @@ router.get('/noticias', async function (req, res, next) {
     }
   });
   res.json(noticias);
+});
+
+router.post('/contacto', async(req, res) => {
+  const mail={
+    to: 'jonathanfernandez88@hotmail.com', 
+    subject:'Contacto web', 
+    html: `${req.body.nombre} se contacto a traves de la web y quiere mas información 
+    a este correo: ${req.body.email} <br> Además, hizo el siguiente comentario: ${req.body.mensaje} 
+    <br> Su tel es: ${req.body.telefono}`
+  }
+  const transport = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
+  });
+
+  await transport.sendMail(mail)
+
+  res.status(201).json({
+    error: false,
+    message: 'Mensaje enviado'
+
+  });
+
 });
 
 module.exports = router;
--- tusfondos/frontend/src/pages/ContactoPage.js
+++ tusfondos/frontend/src/pages/ContactoPage.js
@@ -1,32 +1,81 @@
-import '../styles/ContactoPage.css'
+import '../styles/ContactoPage.css';
+import { useState } from 'react';
+import axios from 'axios';
+
+
 const ContactoPage = (props) => {
+    const initialForm = {
+        nombre: '',
+        email: '',
+        telefono: '',
+        mensaje: ''
+    }
+
+    const [sending, setSending] = useState(false);
+    const [msg, setMsg] = useState('');
+    const [formData, setFormData] = useState(initialForm);
+
+    const handleChange = e => {
+        const { name, value } = e.target;
+        setFormData(oldData => ({
+            ...oldData,
+            [name]: value
+        }));
+    }
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        setMsg('');
+        setSending(true);
+        const response = await axios.post('http://localhost:3000/api/contacto', formData);
+        setSending(false);
+        setMsg(response.data.menssage);
+        if (response.data.error === false) {
+            setFormData(initialForm)
+        }
+
+    }
+
+
     return (
         <main>
             <h2>Formulario de contacto</h2>
             <div className="formulario">
-                <form method='post' action=''>
+                <form method='post' onSubmit={handleSubmit} action='/contacto'>
                     <p>
-                        <label for="nom">Nombre y Apellido</label>
-                        <input type="text" name="nombre_apellido" maxlength="50"/>
+                        <label for="nom">Nombre</label>
+                        <input type="text" name="nombre" value={formData.nombre} onChange={handleChange}/>
                     </p>
                     <p>
                         <label for="nom">Email</label>
-                        <input type="text" name="email" placeholder="@" maxlength="50"/>
+                        <input type="text" name="email" value={formData.email} onChange={handleChange} />
+                    </p>
+                    <p>
+                        <label for="nom">Telefono</label>
+                        <input type="text" name="telefono" value={formData.telefono} onChange={handleChange} />
                     </p>
                     <p>
                         <label for="msj">Mensaje</label>
-                        <textarea type="text" name="email" placeholder="Deje su mensaje aqui..." size="100" maxlength="250"></textarea>
+                        <textarea name="mensaje" value={formData.mensaje} onChange={handleChange}></textarea>
                     </p>
-                <p>
-                    <input type="submit" value="Enviar"/>
-                </p>
-
-
+                    <p>
+                        <input type="submit" value="Enviar" />
+                    </p>
                 </form>
+                {sending ? <p>Enviando ...</p>: null}
+                {msg ? <p>{msg}</p> : null}
+            </div>
+            <div>
+                <h2>Otras vías de comunicación</h2>
+                <p>Tambien puede contactarnos por:</p>
+                <ul>
+                    <li>Telefono 46564151</li>
+                    <li>Email: contacto@tusfondos.com.ar</li>
+                </ul>
             </div>
         </main >
 
     );
-}
+};
 
 export default ContactoPage;
