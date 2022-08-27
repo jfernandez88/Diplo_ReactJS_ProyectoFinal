245b16b9dd0e567e15bbc015b637e9605c933ffb
Jonathan Fernandez <jonathanfernandez88@hotmail.com>
2022-08-27 12:01:46 -0300

    Ajustes esteticos, titulo, logo, botones

--- tusfondos/frontend/src/components/layout/Nav.js
+++ tusfondos/frontend/src/components/layout/Nav.js
@@ -6,8 +6,7 @@ const Nav = (props) => {
             <ul className="holder">
                 <li><NavLink to="/" className={({isActive}) => isActive ? "activo" : undefined}>Inicio</NavLink></li>
                 <li><NavLink to="/NoticiasPage" className={({isActive}) => isActive ? "activo" : undefined}>Noticias</NavLink></li>
-                <li><NavLink to="/FondosPage" className={({isActive}) => isActive ? "activo" : undefined}>Fondos</NavLink></li>
-                <li><NavLink to="/DestacadosPage" className={({isActive}) => isActive ? "activo" : undefined}>DestacadosPage</NavLink></li>
+                <li><NavLink to="/DestacadosPage" className={({isActive}) => isActive ? "activo" : undefined}>Destacados</NavLink></li>
                 <li><NavLink to="/ContactoPage" className={({isActive}) => isActive ? "activo" : undefined}>Contacto</NavLink></li>
 
             </ul>
